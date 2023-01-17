491789468deb7151d69bfc71534a45e574fbe70f
nvencer98 <vencer98@gmail.com>
2023-01-16 19:24:47 -0800

    completed assesment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -22,13 +22,39 @@ const secretCodeWord3 = "Eccentric"
 // Expected output: "3cc3ntr1c"
 
 
-const codeMachine = (value) => {
-const coded = value.replace(/a|A/g,"4").replace(/i|I/g,"1").replace(/o|O/g,"0").replace(/e|E/g,"3")
-return coded
-}
-console.log(codeMachine(secretCodeWord1))
-console.log(codeMachine(secretCodeWord2))
-console.log(codeMachine(secretCodeWord3))
+// CODE STARTS HERE
+
+// describe("codemachine", () => {
+//     it("returns coded words", () => {
+//       expect(codeMachine(secretCodeWord1)).toEqual("L4ck4d41s1c4l")
+//     })
+//   })
+//   describe("codemachine", () => {
+//     it("returns coded words", () => {
+//       expect(codeMachine(secretCodeWord2)).toEqual("G0bbl3dyg00k")
+//     })
+//   })
+//   describe("codemachine", () => {
+//     it("returns coded words", () => {
+//       expect(codeMachine(secretCodeWord3)).toEqual("3cc3ntr1c")
+//     })
+//   })
+
+
+
+// const codeMachine = (value) => {
+// const coded = value.replace(/a/gi,"4").replace(/i/gi,"1").replace(/o/gi,"0").replace(/e/gi,"3")
+// return coded
+// }
+// console.log(codeMachine(secretCodeWord1))
+// console.log(codeMachine(secretCodeWord2))
+// console.log(codeMachine(secretCodeWord3))
+
+
+
+// CODE ENDS HERE!!
+
+
 
 // b) Create the function that makes the test pass.
 
@@ -45,15 +71,37 @@ const letterE = "e"
 
 // b) Create the function that makes the test pass.
 
-const finder = (array,letter) =>{
-    s
-const newArray = array.filter(letter)
-return newArray
+// psudeo code:
+// input: array and single letter
+// output array of words containing letter 
+// create function, filter the array to find letter , identify which strings include the letter, return those in new array. 
 
-}
-console.log(finder(fruitArray,letterA))
+// CODE STARTS HERE
+
+// describe("finder", () => {
+//     it("returns words with letter", () => {
+//       expect(finder(fruitArray,letterA)).toEqual(["Mango", "Apricot", "Peach"])
+//     })
+//   })
+//   describe("finder", () => {
+//     it("returns words with letter", () => {
+//       expect(finder(fruitArray,letterE)).toEqual(["Cherry", "Blueberry", "Peach"])
+//     })
+//   })
+ 
 
 
+
+// const finder = (array,letter) =>{
+//  return array.filter((array) => array.toLowerCase().includes(letter))
+
+
+// }
+// console.log(finder(fruitArray,letterA))
+// console.log(finder(fruitArray,letterE))
+
+
+// CODE ENDS HERE 
 
 // --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a "full house". A full house is exactly one pair and one three of a kind.
 
@@ -69,3 +117,47 @@ const hand4 = [7, 2, 7, 2, 7]
 // Expected output: true
 
 // b) Create the function that makes the test pass.
+
+// psudeo code: 
+// INPUT: ARRAY
+//OUTPUT: boolean true or false.
+// creat functin that takes in array, identifies like numbers, split array into 2 arrays, if 2 arrays equal a pair of 3 and pair of 2 return true, else return false. 
+
+
+describe("fullHouse", () => {
+    it("returns if a full house", () => {
+      expect(fullHouse(hand1)).toEqual(true)
+    })
+  })
+  describe("fullHouse", () => {
+    it("returns if a full house", () => {
+      expect(fullHouse(hand2)).toEqual(false)
+    })
+  })
+  describe("fullHouse", () => {
+    it("returns if a full house", () => {
+      expect(fullHouse(hand3)).toEqual(false)
+    })
+  })
+  describe("fullHouse", () => {
+    it("returns if a full house", () => {
+      expect(fullHouse(hand4)).toEqual(true)
+    })
+  })
+
+
+
+
+
+const fullHouse = (arr) => {
+  let set1 = arr.filter(value => value === arr[0])
+  let secondM = arr.find(value => value !== arr[0])
+  let set2 = arr.filter(value => value === secondM)
+  return (set1.length === 2 && set2.length === 3) || (set1.length ===3 && set2.length === 2) ?
+true : false  
+}
+
+console.log(fullHouse(hand1))
+console.log(fullHouse(hand2))
+console.log(fullHouse(hand3))
+console.log(fullHouse(hand4))
--- interview-questions.md
+++ interview-questions.md
@@ -8,44 +8,51 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What does calling super do in a class?
 
-Your answer:
+Your answer: it helps connect the class to its parent. 
 
-Researched answer:
+Researched answer: it is used call superclass methods and superclass constructor 
 
 2. What is a gem?
 
-Your answer:
+Your answer: a gem is a file or block 
 
-Researched answer:
+Researched answer: a gem is a library that contains a specific piece of functionality also any files or assets that can be related to that functionality.
 
 3. What is a relational database? Are there other kinds of databases?
 
-Your answer:
+Your answer: yes there are other kinds of databases. 
 
-Researched answer:
+Researched answer: a collection of linked tables that relate to each other using keys.
 
 4. What are primary keys? Why are they important?
 
-Your answer:
+Your answer: primary keys are used in relational databases to link similar things together. 
 
-Researched answer:
+Researched answer: A primary key is the column that has values that uniquely identify every row in a table. A database table must have a primary key to insert, restore, update, or delete data from a database table.
 
 5. What are the HTTP verbs? What is each verb's corresponding CRUD action?
 
-Your answer:
+Your answer: create, read, update, delete.
 
-Researched answer:
+Researched answer: POST, GET, PUT, PATCH, and DELETE. create, read, update, delete.
+
+POST - create
+GET - read
+PUT, PATCH - update
+DELETE - delete
+
+
 
 ## Looking Ahead: Terms for Next Week
 
 Research and define the following terms to the best of your ability.
 
-1. RESTful routes:
+1. RESTful routes: maps HTTP methods
 
-2. Model validations:
+2. Model validations: makes sure the model achiebed its intended purpose.
 
-3. Params:
+3. Params: you can specify a method parameter that takes a variable number of arguments.
 
-4. ERB:
+4. ERB:  A templating engine allows you to mix HTML & Ruby so you can generate web pages using data from your database. 
 
-5. API:
+5. API:  a subset of a normal Rails application, created for applications that don't require all functionality that a complete Rails application provides.
